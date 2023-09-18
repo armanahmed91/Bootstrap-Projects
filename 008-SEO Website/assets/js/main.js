51355e01507041786157e4a78a409ba1b83a6f39
arman_ahmed <mdarmanahmed.ar@gmail.com>
2023-09-18 23:10:49 +0600

    Project 8 Done

---
+++ 008-SEO Website/assets/js/main.js
@@ -0,0 +1,19 @@
+// Header Scroll
+let navBar = document.querySelector('.navbar');
+window.onscroll = ()=>{
+    if(document.documentElement.scrollTop > 20){
+        navBar.classList.add("header-scrolled");
+    }
+    else{
+        navBar.classList.remove("header-scrolled");
+    }
+}
+
+// Nav Hide
+let navLink = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse");
+navLink.forEach((a)=>{
+    a.addEventListener("click",()=>{
+        navCollapse.classList.remove("show");
+    })
+})
